b9db5678549117dbcc1b8c175940a09ec04e62a7
joel-noronha <joel.noronha0912@gmail.com>
2025-02-28 21:15:58 +0530

    header added

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import Search from "./components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = React.useState('');
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
+        <main>
+                <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle. </h1>
+                </header>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+        </main>
     )
 }
 export default App
